e310beb9ca1f677871f45dc07432d8cc622c48de
Kiefer <kiefer.katovich@generalassemb.ly>
2016-09-26 13:52:00 -0700

    erased old code

--- dfply/join.py
+++ dfply/join.py
@@ -1,30 +1,48 @@
 from .base import *
 
 
-def get_join_cols(by):
-    """
-    (Idea taken from dplython)
-    """
+def get_join_parameters(join_kwargs):
+    by = kwargs.get('by', None)
+    suffixes = kwargs.get('suffixes', ('_x', '_y'))
     if by is None:
-        return None, None
-    left = [col if isinstance(col, str) else col[0] for col in by]
-    right = [col if isinstance(col, str) else col[1] for col in by]
-    return left, right
+        left_on, right_on = None, None
+    else:
+        left_on = [col if isinstance(col, str) else col[0] for col in by]
+        right_on = [col if isinstance(col, str) else col[1] for col in by]
+    return left_on, right_on, suffixes
 
 
-# def join_function(f):
-#     return Pipe(
-#         SymbolicReference(f)
-#     )
-#
-# @join_function
-# def inner_join(df, other, **kwargs):
-#     left_on, right_on = get_join_cols(kwargs.get('by', None))
-#     suffixes = kwargs.get('suffixes', ('_x', '_y'))
-#     grouped_by = getattr(args[0], '_grouped_by', None)
-#
-#     joined = df.merge(other, how='inner', left_on=left_on,
-#                       right_on=right_on, suffixes=suffixes)
-#     if grouped_by is not None:
-#         joined._grouped_by = grouped_by
-#     return joined
+@Pipe
+def inner_join(df, other, **kwargs):
+    left_on, right_on, suffixes = get_join_parameters(kwargs)
+    joined = df.merge(other, how='inner', left_on=left_on,
+                      right_on=right_on, suffixes=suffixes)
+    return joined
+
+@Pipe
+def full_join(df, other, **kwargs):
+    left_on, right_on, suffixes = get_join_parameters(kwargs)
+    joined = df.merge(other, how='outer', left_on=left_on,
+                      right_on=right_on, suffixes=suffixes)
+    return joined
+
+@Pipe
+def outer_join(df, other, **kwargs):
+    left_on, right_on, suffixes = get_join_parameters(kwargs)
+    joined = df.merge(other, how='outer', left_on=left_on,
+                      right_on=right_on, suffixes=suffixes)
+    return joined
+
+@Pipe
+def left_join(df, other, **kwargs):
+    left_on, right_on, suffixes = get_join_parameters(kwargs)
+    joined = df.merge(other, how='left', left_on=left_on,
+                      right_on=right_on, suffixes=suffixes)
+    return joined
+
+@Pipe
+def right_join(df, other, **kwargs):
+    left_on, right_on, suffixes = get_join_parameters(kwargs)
+    joined = df.merge(other, how='right', left_on=left_on,
+                      right_on=right_on, suffixes=suffixes)
+    return joined
