06662d3e1a988bfdee2f29f78a8a7a43d7229c15
Kiefer <kiefer.katovich@generalassemb.ly>
2016-10-05 10:16:07 -0700

    moved to version 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'dfply',
-    version = '0.1.1',
+    version = '0.1.2',
     author = 'Kiefer Katovich',
     author_email = 'kiefer.katovich@gmail.com',
     packages = [
