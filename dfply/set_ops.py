f35f34b46efdb3c1b8259bc2d4f8f5f05d6084e9
goodtimeslim <bennyska@gmail.com>
2016-10-08 18:59:34 -0700

    Add union

---
+++ dfply/set_ops.py
@@ -0,0 +1,37 @@
+from .base import *
+import warnings
+
+# ==============================================================================
+#
+# SET OPERATIONS
+#
+# These functions treat DataFrames like sets
+#
+# ==============================================================================
+
+
+# ------------------------------------------------------------------------------
+# `union`
+# ------------------------------------------------------------------------------
+
+@pipe
+def union(df, other, index=False, keep='first'):
+    if df.columns.values.tolist() != other.columns.values.tolist():
+        not_in_df = [col for col in other.columns if col not in df.columns]
+        not_in_other = [col for col in df.columns if col not in other.columns]
+        error_string = 'Error: not compatible.'
+        if len(not_in_df):
+            error_string += ' Cols in y but not x: ' + str(not_in_df) + '.'
+        if len(not_in_other):
+            error_string += ' Cols in x but not y: ' + str(not_in_other) + '.'
+        raise ValueError(error_string)
+    stacked = df.append(other)
+    if index:
+        stacked_reset_indexes = stacked.reset_index()
+        index_cols = [col for col in stacked_reset_indexes.columns if col not in df.columns]
+        index_name = df.index.names
+        return_df = stacked_reset_indexes.drop_duplicates(keep=keep).set_index(index_cols)
+        return_df.index.names = index_name
+        return return_df
+    else:
+        return stacked.drop_duplicates(keep=keep)
