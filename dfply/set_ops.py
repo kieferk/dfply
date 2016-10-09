0b163ed7c82162f329b43cbbbd12f8b3f75044ac
goodtimeslim <bennyska@gmail.com>
2016-10-08 22:33:02 -0700

    Add intersect function

--- dfply/set_ops.py
+++ dfply/set_ops.py
@@ -16,6 +16,9 @@ import pandas as pd
 # ------------------------------------------------------------------------------
 
 def validate_set_ops(df, other):
+    # ensure that dataframes are valid for set operations:
+    #   columns must be the same name in the same order
+    #   indexes must be of the same dimension with the same names
     if df.columns.values.tolist() != other.columns.values.tolist():
         not_in_df = [col for col in other.columns if col not in df.columns]
         not_in_other = [col for col in df.columns if col not in other.columns]
@@ -46,3 +49,28 @@ def union(df, other, index=False, keep='first'):
         return return_df
     else:
         return stacked.drop_duplicates(keep=keep)
+
+
+@pipe
+def intersect(df, other, index=False, keep='first'):
+    validate_set_ops(df, other)
+    if index:
+        df_reset_index = df.reset_index()
+        other_reset_index = other.reset_index()
+        index_cols = [col for col in df_reset_index.columns if col not in df.columns]
+        df_index_names = df.index.names
+        return_df = (pd.merge(df_reset_index, other_reset_index,
+                              how='inner',
+                              left_on=df_reset_index.columns.values.tolist(),
+                              right_on=df_reset_index.columns.values.tolist())
+                     .set_index(index_cols))
+        return_df.index.names = df_index_names
+        return_df = return_df.drop_duplicates(keep=keep)
+        return return_df
+    else:
+        return_df = pd.merge(df, other,
+                             how='inner',
+                             left_on=df.columns.values.tolist(),
+                             right_on=df.columns.values.tolist())
+        return_df = return_df.drop_duplicates(keep=keep)
+        return return_df
