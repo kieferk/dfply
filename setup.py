b0736d75e1bb2dd65ab7196f5931fd38881d03c5
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2016-10-10 23:27:28 +0200

    moved data out of python package, added setup.py and MANIFEST.in links to data, and made data.py a module that loads the data, to keep code working without API change.  Added pandas_ply and six to requirements, to remove vendor code from python package as well.

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
-#from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'dfply',
     version = '0.1.6',
     author = 'Kiefer Katovich',
     author_email = 'kiefer.katovich@gmail.com',
-    packages = [
-        'dfply',
-        'dfply.vendor',
-        'dfply.vendor.pandas_ply',
-        'dfply.data'
-    ],
+    packages = find_packages(),
+    include_package_data=True,
+    package_data={'': '../data/*.csv'},
+    install_requires=['numpy', 'pandas', 'six', 'pandas_ply'],
     description = 'dplyr-style piping operations for pandas dataframes',
     long_description = 'See https://github.com/kieferk/dfply/blob/master/README.md for details.',
     license = 'GNU General Public License v3.0',
-    url = 'https://github.com/kieferk/dfply'
+    url = 'https://github.com/kieferk/dfply',
+    test_suite='tests',
 )
