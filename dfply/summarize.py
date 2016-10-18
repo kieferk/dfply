a0f40d71e9ff34aaa21ef07d1a015fcb3e5df128
Kiefer <kiefer.katovich@generalassemb.ly>
2016-10-17 17:49:38 -0700

    moved series summary functions to summarize.py

--- dfply/summarize.py
+++ dfply/summarize.py
@@ -28,11 +28,60 @@ def summarize_each(df, functions, *args):
     return pd.DataFrame([values], columns=columns)
 
 
-# @dfpipe
-# def count(df):
-#     return pd.DataFrame(dict(n=[df.shape[0]]))
-#
-#
-# @Pipe
-# def nrow(df):
-#     return df.shape[0]
+# ------------------------------------------------------------------------------
+# Series summary functions
+# ------------------------------------------------------------------------------
+
+def mean(series):
+    mean_s = series.mean()
+    return mean_s
+
+
+def first(series):
+    first_s = series.iloc[0]
+    return first_s
+
+
+def last(series):
+    last_s = series.iloc[series.size - 1]
+    return last_s
+
+
+def n(series):
+    n_s = series.size
+    return n_s
+
+
+def n_distinct(series):
+    n_distinct_s = series.unique().size
+    return n_distinct_s
+
+
+def IQR(series):
+    iqr_s = series.quantile(.75) - series.quantile(.25)
+    return iqr_s
+
+
+def min(series):
+    min_s = series.min()
+    return min_s
+
+
+def max(series):
+    max_s = series.max()
+    return max_s
+
+
+def median(series):
+    median_s = series.median()
+    return median_s
+
+
+def var(series):
+    var_s = series.var()
+    return var_s
+
+
+def sd(series):
+    sd_s = series.std()
+    return sd_s
