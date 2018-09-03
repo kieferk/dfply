d1e42c2941dafa063d86786477c38484440f98af
Jan Kislinger <jan.kislinger@gmail.com>
2018-09-03 14:30:10 +0200

    fixed typo in code

--- dfply/join.py
+++ dfply/join.py
@@ -21,7 +21,7 @@ def get_join_parameters(join_kwargs):
         else:
             if not isinstance(by[0], str):
                 left_on = by[0]
-                right_in = by[1]
+                right_on = by[1]
             else:
                 left_on, right_on = by, by
     return left_on, right_on, suffixes
